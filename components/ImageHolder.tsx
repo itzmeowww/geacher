65428acf9e21f9dde6d51280fee5442cd630d7c9
itzmeowww <winthanasan@gmail.com>
2022-03-16 17:12:19 +0700

    update loading animation

--- components/ImageHolder.tsx
+++ components/ImageHolder.tsx
@@ -1,42 +1,42 @@
 import Image from 'next/image'
 
 type ImageHolderProps = {
-    url: string
-    alt: string
-    width: number
-    height: number
+  url: string
+  alt: string
+  width: number
+  height: number
 }
 
 
 const ImageHolder = ({ url, alt, width, height }: ImageHolderProps) => {
 
 
-    const convertImage = (w, h) => `
+  const convertImage = (w, h) => `
     <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <defs>
         <linearGradient id="g">
-          <stop stop-color="#333" offset="20%" />
-          <stop stop-color="#222" offset="50%" />
-          <stop stop-color="#333" offset="70%" />
+          <stop stop-color="#e2e8f0" offset="20%" />
+          <stop stop-color="#cbd5e1" offset="50%" />
+          <stop stop-color="#e2e8f0" offset="70%" />
         </linearGradient>
       </defs>
-      <rect width="${w}" height="${h}" fill="#333" />
+      <rect width="${w}" height="${h}" fill="#e2e8f0" />
       <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
       <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
     </svg>`;
 
-    const toBase64 = (str) =>
-        typeof window === 'undefined'
-            ? Buffer.from(str).toString('base64')
-            : window.btoa(str);
-    return (
-        <div className='w-full h-full' >
-            <Image alt={alt} src={url} width={width} height={height} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                convertImage(width, height)
-            )}`} />
-        </div>
+  const toBase64 = (str) =>
+    typeof window === 'undefined'
+      ? Buffer.from(str).toString('base64')
+      : window.btoa(str);
+  return (
+    <div className='w-full h-full' >
+      <Image alt={alt} src={url} width={width} height={height} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${toBase64(
+        convertImage(width, height)
+      )}`} />
+    </div>
 
-    );
+  );
 }
 
 
