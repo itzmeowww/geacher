8c2f50c593aca8bc6bf15534742cf37fbf9c4c1a
itzmeowww <winthanasan@gmail.com>
2023-02-24 12:08:03 +0900

    update

--- data-prep/image.py
+++ data-prep/image.py
@@ -5,7 +5,7 @@ path = os.getcwd()
 
 path = os.path.join(path, 'public', 'posters')
 
-target_file = "6m403.jpg"
+target_file = "5m313.jpg"
 
 for name in os.listdir(path):
     if target_file == None or target_file == name:
